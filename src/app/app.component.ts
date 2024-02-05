afd478f39c58168fb1d590c6c8b1ddbd5d16955c
siharfos <si.acerola@gmail.com>
2024-02-05 15:19:27 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    PostCreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
