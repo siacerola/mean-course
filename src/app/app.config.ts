edab4f4398cb7dea530f99687cb61c35eb62fd20
sihar <luq5nvd@gmail.com>
2024-02-05 19:42:15 +0700

    installing angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,9 @@ import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes)]
+    provideRouter(routes), provideAnimationsAsync()]
 };
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from "@angular/material/card";
+
+
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 })
