caa5a900daa09ab4c24513f116e29bef5afcb977
siharfos <si.acerola@gmail.com>
2024-02-07 14:14:58 +0700

    connectiong our node express app to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://sihar:HtcvdoVxADwcbSOY@cluster0.cwxk3fs.mongodb.net/?retryWrites=true&w=majority").then(() => {
+    console.log('connected to database');
+}).catch(() => {
+    console.log('connection failed');
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
