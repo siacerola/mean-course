48f15f8c8c491ec82b91b0d1efaf52ba761b1233
siharfos <si.acerola@gmail.com>
2024-02-07 14:59:50 +0700

    updating the frontend after deleting posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -55,9 +55,11 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    this.http.delete(`http://localhost:3000/api/posts/${postId}`)
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
       .subscribe(() => {
-        console.log('deleted');
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
 
       })
   }
